a89a5a91f0a61a473f9d34887da8c209c833d88e
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2019-07-21 22:01:34 +0800

    update chromium search algorythm

--- scripts/findChrome.js
+++ scripts/findChrome.js
@@ -1,219 +1,108 @@
-const fs = require('fs')
 const path = require('path')
-const { homedir } = require('os')
+const ProgressBar = require('progress')
+const { execSync } = require('child_process')
+
 const puppeteer = require('puppeteer-core')
-const { execSync, execFileSync } = require('child_process')
 const pptrCoreJson = require('puppeteer-core/package.json')
 const { writeFile } = require('../src/utils')
 
-const newLineRegex = /\r?\n/
 const chromeTempPath = path.join(process.cwd(), 'temp', 'chrome')
-const chromeConfigPath = path.join(process.cwd(), 'chrome.json')
+const downloadHost = process.env.PUPPETEER_DOWNLOAD_HOST || process.env.npm_config_puppeteer_download_host || process.env.npm_package_config_puppeteer_download_host
+const browserFetcher = puppeteer.createBrowserFetcher({ host: downloadHost, path: chromeTempPath })
 
-function canAccess(file) {
-  if (!file) {
-    return false
+const revision = process.env.PUPPETEER_CHROMIUM_REVISION || pptrCoreJson.puppeteer.chromium_revision
+
+function logPolitely(toBeLogged) {
+  const logLevel = process.env.npm_config_loglevel
+  const logLevelDisplay = ['silent', 'error', 'warn'].indexOf(logLevel) > -1
+
+  if (!logLevelDisplay)
+    console.log(toBeLogged)
+}
+
+async function findChromium() {
+  const revisionInfo = browserFetcher.revisionInfo(revision)
+  if (revisionInfo.local) {
+    logPolitely(`Chromium r${revision} found local at ${revisionInfo.executablePath}`)
+    return revisionInfo
   }
 
-  try {
-    fs.accessSync(file)
-    return true
-  } catch (e) {
-    return false
-  }
+  throw new Error(`Chromium r${revision} not found local`)
 }
 
-function uniq(arr) {
-  return Array.from(new Set(arr))
+function toMegabytes(bytes) {
+  const mb = bytes / 1024 / 1024
+  return `${Math.round(mb * 10) / 10} Mb`
 }
 
-function darwin(canary) {
-  const LSREGISTER =
-    '/System/Library/Frameworks/CoreServices.framework' +
-    '/Versions/A/Frameworks/LaunchServices.framework' +
-    '/Versions/A/Support/lsregister'
-  const grepexpr = canary ? 'google chrome canary' : 'google chrome'
-  const result = execSync(
-    `${LSREGISTER} -dump  | grep -i '${grepexpr}?.app$' | awk '{$1=""; print $0}'`
-  )
-
-  const paths = result
-    .toString()
-    .split(newLineRegex)
-    .filter(a => a)
-    .map(a => a.trim())
-
-  paths.unshift(
-    canary ? '/Applications/Google Chrome Canary.app' : '/Applications/Google Chrome.app'
-  )
-
-  for (const p of paths) {
-    // eslint-disable-next-line no-continue
-    if (p.startsWith('/Volumes')) continue
-    const inst = path.join(
-      p,
-      canary ? '/Contents/MacOS/Google Chrome Canary' : '/Contents/MacOS/Google Chrome'
-    )
-    if (canAccess(inst)) return inst
-  }
-
-  return undefined
-}
-
-function win32(canary) {
-  const suffix = canary
-    ? `${path.sep}Google${path.sep}Chrome SxS${path.sep}Application${path.sep}chrome.exe`
-    : `${path.sep}Google${path.sep}Chrome${path.sep}Application${path.sep}chrome.exe`
-
-  const prefixes = [
-    process.env.LOCALAPPDATA,
-    process.env.PROGRAMFILES,
-    process.env['PROGRAMFILES(X86)'],
-  ].filter(Boolean)
-
-  let result
-  prefixes.forEach(prefix => {
-    const chromePath = path.join(prefix, suffix)
-    if (canAccess(chromePath)) result = chromePath
-  })
-
-  return result
-}
-
-function sort(installations, priorities) {
-  const defaultPriority = 10
-  return installations
-    .map(inst => {
-      for (const pair of priorities) {
-        if (pair.regex.test(inst)) return { path: inst, weight: pair.weight }
-      }
-      return { path: inst, weight: defaultPriority }
-    })
-    .sort((a, b) => b.weight - a.weight)
-    .map(pair => pair.path)
-}
-
-function findChromeExecutables(folder) {
-  const installations = []
-  const argumentsRegex = /(^[^ ]+).*/
-  const chromeExecRegex = '^Exec=/.*/(google-chrome|chrome|chromium)-.*'
-
-  if (canAccess(folder)) {
-    let execPaths
-
-    try {
-      execPaths = execSync(`grep -ER "${chromeExecRegex}" ${folder} | awk -F '=' '{print $2}'`)
-    } catch (e) {
-      execPaths = execSync(`grep -Er "${chromeExecRegex}" ${folder} | awk -F '=' '{print $2}'`)
-    }
-
-    execPaths = execPaths
-      .toString()
-      .split(newLineRegex)
-      .map(execPath => execPath.replace(argumentsRegex, '$1'))
-
-    execPaths.forEach(execPath => canAccess(execPath) && installations.push(execPath))
-  }
-
-  return installations
-}
-
-function linux() {
-  let installations = []
-
-  // Look into the directories where .desktop are saved on gnome based distro's
-  const desktopInstallationFolders = [
-    path.join(homedir(), '.local/share/applications/'),
-    '/usr/share/applications/',
-  ]
-
-  desktopInstallationFolders.forEach(folder => {
-    installations = installations.concat(findChromeExecutables(folder))
-  })
-
-  // Look for google-chrome(-stable) & chromium(-browser) executables by using the which command
-  const executables = ['google-chrome-stable', 'google-chrome', 'chromium-browser', 'chromium']
-  executables.forEach(executable => {
-    try {
-      const chromePath = execFileSync('which', [executable], { stdio: 'pipe' })
-        .toString()
-        .split(newLineRegex)[0]
-      if (canAccess(chromePath)) installations.push(chromePath)
-    } catch (e) {
-      // Not installed.
-    }
-  })
-
-  if (!installations.length) {
-    return undefined
-  }
-
-  const priorities = [
-    { regex: /chrome-wrapper$/, weight: 51 },
-    { regex: /google-chrome-stable$/, weight: 50 },
-    { regex: /google-chrome$/, weight: 49 },
-    { regex: /chromium-browser$/, weight: 48 },
-    { regex: /chromium$/, weight: 47 },
-  ]
-
-  if (process.env.CHROME_PATH) {
-    priorities.unshift({
-      regex: new RegExp(`${process.env.CHROME_PATH}`),
-      weight: 101,
+let progressBar = null
+let lastDownloadedBytes = 0
+function onProgress(downloadedBytes, totalBytes) {
+  if (!progressBar) {
+    progressBar = new ProgressBar(`Downloading Chromium r${revision} - ${toMegabytes(totalBytes)} [:bar] :percent :etas `, {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: totalBytes,
     })
   }
-
-  return sort(uniq(installations.filter(Boolean)), priorities)[0]
+  const delta = downloadedBytes - lastDownloadedBytes
+  lastDownloadedBytes = downloadedBytes
+  progressBar.tick(delta)
 }
 
 async function downloadChromium() {
-  const browserFetcher = puppeteer.createBrowserFetcher({
-    path: chromeTempPath,
-  })
-
-  const revision = pptrCoreJson.puppeteer.chromium_revision
-  const revisionInfo = browserFetcher.revisionInfo(revision)
-
-  // If already downloaded
-  if (revisionInfo.local) return revisionInfo
-
-  try {
-    console.log(`Downloading Chromium r${revision}...`)
-    const newRevisionInfo = await browserFetcher.download(revisionInfo.revision)
-    console.log(`Chromium downloaded to ${newRevisionInfo.folderPath}`)
-
-    let localRevisions = await browserFetcher.localRevisions()
-    localRevisions = localRevisions.filter(r => r !== revisionInfo.revision)
-
-    // Remove previous revisions
-    const cleanupOldVersions = localRevisions.map(r => browserFetcher.remove(r))
-    await Promise.all(cleanupOldVersions)
-
-    return newRevisionInfo
-  } catch (error) {
-    console.error(`ERROR: Failed to download Chromium r${revision}!`)
-    console.error(error)
-    return null
-  }
+  return browserFetcher.download(revision, onProgress)
 }
 
-async function findChrome() {
-  let executablePath
-
-  if (process.platform === 'linux') executablePath = linux()
-  else if (process.platform === 'win32') executablePath = win32()
-  else if (process.platform === 'darwin') executablePath = darwin()
-
-  if (typeof executablePath === 'string' && executablePath.length > 0) {
-    await writeFile(chromeConfigPath, JSON.stringify({ executablePath }))
-    console.log(`Local Chrome location: ${executablePath}`)
-    return executablePath
-  }
-
-  const revisionInfo = await downloadChromium()
-  await writeFile(chromeConfigPath, JSON.stringify({ executablePath: revisionInfo.executablePath }))
-  console.log(`Downloaded Chrome location: ${revisionInfo.executablePath}`)
-  return revisionInfo.executablePath
+async function writeChromeConfig(config) {
+  const chromeConfigPath = path.join(process.cwd(), 'chrome.json')
+  await writeFile(chromeConfigPath, JSON.stringify(config))
 }
 
-findChrome()
+function onSuccess(revisionInfo) {
+  const chromiumVersionResult = execSync(
+    `${revisionInfo.executablePath} --version`
+  )
+
+  const chromiumMajorVersion = chromiumVersionResult.toString().match(/Chromium (\d*)\..*/i)[1]
+  if (parseInt(chromiumMajorVersion, 10) < 75) {
+    console.log("ERROR: Estimo doesn't support Chromium version < 75")
+    throw new Error('Chromium version < 75')
+  }
+
+  writeChromeConfig({ executablePath: revisionInfo.executablePath })
+  return revisionInfo
+}
+
+function onSuccessfulDownload(revisionInfo) {
+  logPolitely(`Chromium successfully downloaded to ${revisionInfo.folderPath}`)
+}
+
+function onFailedDownload(error) {
+  console.error(`ERROR: Failed to download Chromium r${revision}! Set "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" env variable to skip download.`)
+  console.error(error)
+  process.exit(1)
+}
+
+if (process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD) {
+  logPolitely('**INFO** Skipping Chromium download. "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" environment variable was found.')
+  process.exit(1)
+}
+if (process.env.NPM_CONFIG_PUPPETEER_SKIP_CHROMIUM_DOWNLOAD) {
+  logPolitely('**INFO** Skipping Chromium download. "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" was set in npm config.')
+  process.exit(1)
+}
+if (process.env.NPM_PACKAGE_CONFIG_PUPPETEER_SKIP_CHROMIUM_DOWNLOAD) {
+  logPolitely('**INFO** Skipping Chromium download. "PUPPETEER_SKIP_CHROMIUM_DOWNLOAD" was set in project config.')
+  process.exit(1)
+}
+
+findChromium()
+.then(onSuccess)
+.catch(() => {
+  downloadChromium()
+  .then(onSuccess)
+  .then(onSuccessfulDownload)
+  .catch(onFailedDownload)
+})
